74f7d089980bd358fa71260651200bcdbadc0c91
Kelvin Duong <89860835+Tundramokous@users.noreply.github.com>
2022-05-18 21:26:17 -0700

    Add files via upload

---
+++ c1_intro_parallel_computing/PythonDemo.py
@@ -0,0 +1,5 @@
+# Creating an example Python application. 
+# You can do it with the %%writefile magic like we are doing here,
+# or you can go to the File menu, choose Open, and from there
+# either upload your code or create a .py file and compose it in the Notebook
+print ("Hello world from Python!")
---
+++ c2_augmenting_application/ParallelCode.py
@@ -0,0 +1,8 @@
+
+print("Replace this with your Python code!!")
+
+def main() -> None:
+    print("Replace this with calls to your functions")
+
+if __name__ == '__main__':
+    print("Replace this with proper timeit calls")
